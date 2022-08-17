bf34e6fcedacca67e3c24336f1e533934bc0982f
aubustou <charlemagnecontrelesninjas@gmail.com>
2022-08-17 15:38:30 +0200

    fix dependencies

--- setup.py
+++ setup.py
@@ -7,7 +7,8 @@ setup(
     author="aubustou",
     author_email="survivalfr@yahoo.fr",
     install_requires=[
-        "PyGObject==3.42.2",
+        "pycairo>=1.21.0",
+        "PyGObject>=3.42.2",
         "python-mercuryapi @ git+https://github.com/gotthardp/python-mercuryapi.git@master",
     ],
     entry_points={
